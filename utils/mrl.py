7bc2a9691f71d14afbb829b37f963382db9958f8
jaehulp <jaehulp@yonsei.ac.kr>
2025-09-03 15:55:34 +0900

    change alpha

--- utils/mrl.py
+++ utils/mrl.py
@@ -2,6 +2,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+"""
+Original code github. @RAIVNLab
+https://github.com/RAIVNLab/MRL
+"""
+
+
 class TopKOutputWrapper(nn.Module):
     def __init__(self, model, classifier_layer_name, k=100, alpha=0.5):
         """
@@ -50,7 +56,7 @@ class TopKOutputWrapper(nn.Module):
             topk_output = self.classifier(topk_features)
 
             # Weighted sum of outputs
-            combined_output = self.alpha * original_output + (1 - self.alpha) * topk_output
+            combined_output = (1-self.alpha) * original_output + self.alpha * topk_output
             return combined_output
         else:
             # Inference: return only original output
@@ -63,6 +69,57 @@ class TopKOutputWrapper(nn.Module):
 from collections import Counter
 
 class PrefixTopKOutputWrapper(TopKOutputWrapper):
+    def __init__(self, model, classifier_layer_name, k=100, alpha=0.5):
+        """
+        model: pretrained backbone + classifier model
+        classifier_layer_name: str, name of the classifier attribute in the model
+        k: number of top-k features
+        alpha: weight for original output
+        """
+        super().__init__(model, classifier_layer_name, k=k, alpha=alpha)
+
+    def set_topk_indices(self, train_loader):
+        self.model.eval()
+        feature_counts = torch.zeros(512, device=self.device)
+
+        for i, (xs, ys) in enumerate(train_loader):
+            xs = xs.to(self.device)
+            _  = self.model(xs)
+
+            features = self.features
+
+            topk_indices = torch.topk(features, self.k, dim=1).indices  # [B, k]
+
+            for i in range(topk_indices.shape[0]):
+                feature_counts.scatter_add_(
+                    0,
+                    topk_indices[i],
+                    torch.ones_like(topk_indices[i], dtype=feature_counts.dtype)
+                )
+
+        _, global_topk_indices = torch.topk(feature_counts, self.k)
+        
+        mask = torch.zeros_like(feature_counts, device=self.device)
+        mask.scatter_(0, global_topk_indices, 1.0)
+        mask = mask.unsqueeze(dim=0)
+        self.mask = mask
+
+    def forward(self, x):
+        x = x.to(self.device)
+        original_output = self.model(x)
+
+        if self.training:
+            topk_features = self.features.clone()
+            topk_features = topk_features * self.mask
+
+            topk_output = self.classifier(topk_features)
+
+            combined_output = (1-self.alpha) * original_output + self.alpha * topk_output
+            return combined_output
+        else:
+            return original_output
+
+class RandPrefixTopKOutputWrapper(TopKOutputWrapper):
     def __init__(self, model, classifier_layer_name, k=100, alpha=0.5):
         """
         model: pretrained backbone + classifier model
